c1731ec26e790852206ee65451ac72554f0cc970
Alexandre Sanches <alsanches@gmail.com>
2020-11-07 11:04:41 -0300

    Primeira alteração

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    LeadsControl
                 </div>
 
                 <div class="links">
